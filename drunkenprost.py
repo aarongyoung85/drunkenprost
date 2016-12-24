ea3e2bde98fd41a49c970ad424fbb8ebf812d4eb
aarongyoung85 <aarongyoung85@gmail.com>
2016-12-24 09:53:44 -0800

    adding main html templates and main.css

--- drunkenprost.py
+++ drunkenprost.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+logger = app.logger
 
 @app.route('/')
 def index():
+    logger.error("test logging")
     return render_template('index.html')
     
 if __name__ == '__main__':

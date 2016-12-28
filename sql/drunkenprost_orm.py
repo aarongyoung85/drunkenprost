a84e3182ee2bda9bf62b63be60a168d3df61300a
aarongyoung85 <aarongyoung85@gmail.com>
2016-12-28 14:36:20 -0800

    add wine tables

--- sql/drunkenprost_orm.py
+++ sql/drunkenprost_orm.py
@@ -22,12 +22,30 @@ class BeerTypes(Base):
     id = Column(String, primary_key=True)
     beer_type = Column(String)
 
+class Wineries(Base):
+    __tablename__ = 'wineries'
+
+    id = Column(String, primary_key=True)
+    winery = Column(String)
+
+class WineTypes(Base):
+    __tablename__ = 'wine_types'
+
+    id = Column(String, primary_key=True)
+    wine_type = Column(String)
+
 class Countries(Base):
     __tablename__ = 'countries'
 
     id = Column(String, primary_key=True)
     iso = Column(String)
     country = Column(String)
+
+class Regions(Base):
+    __tablename__ = 'regions'
+
+    id = Column(String, primary_key=True)
+    region = Column(String)
 
 class Beers(Base):
     __tablename__ = 'beers'
@@ -41,6 +59,21 @@ class Beers(Base):
     link_name = Column(String, index=True)
     display_name = Column(String)
 
+class Wines(Base):
+    __tablename__ = 'wines'
+
+    id = Column(String, primary_key=True)
+    created = Column(DateTime)
+    winery = Column(String, ForeignKey('Wineries.id'))
+    wine_type = Column(String, ForeignKey('WineTypes.id'))
+    country = Column(String, ForeignKey('Countries.id'))
+    vintage = Column(Integer)
+    region = Column(String, ForeignKey('Regions.id'))
+    abv = Column(Float(precision=3, scale=1, asdecimal=True))
+    link_name = Column(String, index=True)
+    display_name = Column(String)
+
 topics_map = {
-    'beer': Beers
+    'beer': Beers,
+    'wine': Wines
 }
